57304987be24c1c6f74a5b695fb830c46e32aa31
Juan Sebastián <89955811+juansebastian2006@users.noreply.github.com>
2021-10-25 22:16:16 -0300

    Create insertion.py

---
+++ insertion.py
@@ -0,0 +1,15 @@
+def insert(lista: list, index: int, item):
+
+  """Insert the given item to your list"""
+
+  if index == -1 or index >= len(lista):
+     lista.append(item)
+     return lista
+
+  new_list = [item]
+
+  for i in range(index, len(lista)):
+     new_list.append(lista[i])
+
+  lista = lista[: index]
+  return lista + new_list
